577216ef3a49b3ffd6b33cfcae872ec1aca74198
SaulAlejandroMamaniAlania <smamanialania@est.frba.utn.edu.ar>
2018-08-17 20:45:22 -0300

    baseDeConocimiento

---
+++ ParcialHearthstone.pl
@@ -0,0 +1,12 @@
+% jugadores
+jugador(Nombre, PuntosVida, PuntosMana, 
+CartasMazo, CartasMano, CartasCampo)
+
+% cartas
+criatura(Nombre, PuntosDaño, PuntosVida, CostoMana)
+hechizo(Nombre, FunctorEfecto, CostoMana)
+
+% efectos
+daño(CantidadDaño)
+cura(CantidadCura)
+
